80cf1b2f84321c8f928d495702f71672b0fe05fb
supermancbs <clark.sacktor@gmail.com>
2016-06-27 19:07:03 -0400

    Done.

--- app.rb
+++ app.rb
@@ -13,5 +13,15 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get '/goodbye/:name' do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}"
+  end 
+
+  get '/multiply/:num1/:num2' do 
+    (params[:num1].to_i * params[:num2].to_i).to_s
+
+  end 
+
 
 end
